6e4a9856c5b78f954534a9bb2b8215b4d5a6afd0
Tim Taylor <ttaylor@clarion.edu>
2017-02-12 18:47:16 -0500

    Testing TorF that the lenght must be 10 digits for isbn10

---
+++ isbn_new.rb
@@ -0,0 +1,7 @@
+def isbn10_length?(isbn_num)
+	if isbn_num.length == 10
+		true
+	else
+		false
+	end
+end
--- test_isbn_new.rb
+++ test_isbn_new.rb
@@ -7,5 +7,15 @@ class TestIsbnNew < Minitest::Test
 		assert_equal(1,1)
 	end
 
+	def test_10_digits_true
+		isbn_num = '1234567890'
+		assert_equal(true, isbn10_length?(isbn_num))
+	end
+
+	def test_8_digits_false
+		isbn_num = '12345678'
+		assert_equal(false, isbn10_length?(isbn_num))
+	end	
+
 end
 
